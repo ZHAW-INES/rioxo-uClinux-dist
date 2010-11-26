ff61e96addb6de0ea647d3b3cadee7530b2c82b9
Thomas Stanger (stth) <stth@zhaw.ch>
2010-11-26 08:48:58 +0100

    hdoip_core: new ioctl commands added and misc changes

--- vendors/InES/eva/modules/include/hdoip_types.h
+++ vendors/InES/eva/modules/include/hdoip_types.h
@@ -12,18 +12,31 @@
  * Parameter struct to pass to hdoip_core device. Needs to be packed because it
  * is transfered between user and kernel space.
  */
+
 struct hdoip_core_param {
 	__u32 bar;
 	__u16 foo;
 	__u8 baz;
-} __packed;
-
+	__u8 i2c_reg_map; 
+	__u8 i2c_addr;
+	__u8 i2c_value;
+} __attribute__((packed));
 /* ioctl command definitions */
 
 #define HDOIP_IOCTL_MAGIC	'H'
 
+#define HDOIP_ADV7441A_GET		_IOWR(HDOIP_IOCTL_MAGIC, 1, struct hdoip_core_param)
+#define HDOIP_ADV7441A_SET		_IOW(HDOIP_IOCTL_MAGIC, 2, struct hdoip_core_param)
+#define HDOIP_ADV7441A_INIT		_IO(HDOIP_IOCTL_MAGIC, 3)
+#define HDOIP_ADV7441A_RESET		_IO(HDOIP_IOCTL_MAGIC, 4)
+#define HDOIP_ADV7441A_ON		_IO(HDOIP_IOCTL_MAGIC, 5)
+#define HDOIP_ADV7441A_OFF		_IO(HDOIP_IOCTL_MAGIC, 6)
+#define HDOIP_ADV7441A_AUDIO_MUTE	_IO(HDOIP_IOCTL_MAGIC, 7)
+#define HDOIP_ADV7441A_AUDIO_UNMUTE	_IO(HDOIP_IOCTL_MAGIC, 8)
+#define HDOIP_ADV7441A_HANDLER		_IO(HDOIP_IOCTL_MAGIC, 9)
+/*
 #define HDOIP_IOCRESET		_IO(HDOIP_IOCTL_MAGIC, 1)
 #define HDOIP_IOCGETPARAM	_IOR(HDOIP_IOCTL_MAGIC, 2, struct hdoip_core_param)
 #define HDOIP_IOCSETPARAM	_IOW(HDOIP_IOCTL_MAGIC, 3, struct hdoip_core_param)
-
+*/
 #endif /* _HDOIP_TYPES_H_ */
