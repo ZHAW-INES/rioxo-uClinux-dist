33b080fcf2030971b55e1b3f27f64067e5caf8f0
Thomas Stanger (stth) <stth@zhaw.ch>
2011-05-02 16:07:42 +0200

    hdoip_daemon: removed unused messages.

--- user/hd_over_ip/hdoip_daemon/hdoipd_msg.h
+++ user/hd_over_ip/hdoip_daemon/hdoipd_msg.h
@@ -18,6 +18,10 @@
 #include <err.h>
 #include <errno.h>
 
+#define PACK  		__attribute__ ((packed))
+#define ALIGN4
+//__attribute__ ((align(4)))
+
 //------------------------------------------------------------------------------
 // Commands
 
@@ -34,8 +38,6 @@
 #define HOIC_VRB_PLAY           (0x31000007)
 #define HOIC_READY              (0x31000008)
 #define HOIC_REBOOT             (0x31000009)
-#define HOIC_LD_CFG             (0x3100000a)
-#define HOIC_LD_FLASH           (0x3100000b)
 #define HOIC_REMOTE_UPDATE      (0x3100000c)
 #define HOIC_GETVERSION         (0x3100000d)
 #define HOIC_REPAIR             (0x3100000e)
@@ -57,10 +59,6 @@
 // Config
 #define HOIC_PARAM_GET          (0x3800000a)
 #define HOIC_PARAM_SET          (0x3800000b)
-
-// Flash
-#define HOIC_FLASH_READ         (0x39000001)
-#define HOIC_FLASH_WRITE        (0x39000002)
 
 //------------------------------------------------------------------------------
 //
@@ -107,7 +105,7 @@ static inline ssize_t hoic_rdpipe(int rsp, uint8_t *buf, size_t size)
 
 typedef struct {
     uint32_t            msgid, msgsize;
-} __attribute__ ((packed)) t_hoic_cmd;
+} PACK t_hoic_cmd ALIGN4;
 
 static inline void hoic_sw(int fd, uint32_t id)
 {
@@ -130,31 +128,14 @@ static inline void hoic_sw(int fd, uint32_t id)
 
 
 
+
 //------------------------------------------------------------------------------
 // flash 
 typedef struct {
     uint32_t            msgid, msgsize;
     uint32_t            offset;
     uint32_t            value;
-} __attribute__ ((packed)) t_hoic_flash;
-
-
-static inline void hoic_flash_read(int fd, uint32_t offset)
-{
-    hoic_setup(t_hoic_flash, HOIC_FLASH_READ);
-//    tmp.value = value;
-    tmp.offset = offset;
-    hoic_write(t_hoic_flash);
-}
-
-static inline void hoic_flash_write(int fd, uint32_t offset, uint32_t value)
-{
-    hoic_setup(t_hoic_flash, HOIC_FLASH_WRITE);
-    tmp.value = value;
-    tmp.offset = offset;
-    hoic_write(t_hoic_flash);
-}
-
+} PACK t_hoic_flash ALIGN4;
 
 //------------------------------------------------------------------------------
 // attribute/parameter commands
@@ -162,7 +143,7 @@ static inline void hoic_flash_write(int fd, uint32_t offset, uint32_t value)
 typedef struct {
     uint32_t            msgid, msgsize;
     uint32_t            value;
-} __attribute__ ((packed)) t_hoic_param;
+} PACK t_hoic_param ALIGN4;
 
 
 
@@ -198,7 +179,7 @@ static inline void hoic_ret_read(int fd, uint32_t value)
 typedef struct {
     uint32_t            msgid, msgsize;
     uint32_t            width, height, fps;
-} __attribute__ ((packed)) t_hoic_canvas;
+} PACK t_hoic_canvas ALIGN4;
 
 static inline void hoic_canvas(int fd, uint32_t width, uint32_t height, uint32_t fps)
 {
@@ -214,7 +195,7 @@ static inline void hoic_canvas(int fd, uint32_t width, uint32_t height, uint32_t
 typedef struct {
     uint32_t            msgid, msgsize;
     char                filename[];
-} __attribute__ ((packed)) t_hoic_load;
+} PACK t_hoic_load ALIGN4;
 
 static inline void hoic_load(int fd, char* filename)
 {
@@ -232,24 +213,6 @@ static inline void hoic_vrb_setup(int fd, char* uri)
     write(fd, uri, length);
 }
 
-static inline void hoic_ld_cfg(int fd, char* filename)
-{
-    size_t length = strlen(filename) + 1;
-    hoic_setupx(t_hoic_load, HOIC_LD_CFG, length);
-    hoic_write(t_hoic_load);
-    write(fd, filename, length);
-}
-
-static inline void hoic_ld_flash(int fd, char* filename)
-{
-    size_t length = strlen(filename) + 1;
-    hoic_setupx(t_hoic_load, HOIC_LD_FLASH, length);
-    hoic_write(t_hoic_load);
-    write(fd, filename, length);
-}
-
-
-
 //------------------------------------------------------------------------------
 // parameter
 
@@ -257,7 +220,7 @@ typedef struct {
     uint32_t            msgid, msgsize;
     size_t              offset;
     char                str[];
-} __attribute__ ((packed)) t_hoic_kvparam;
+} PACK t_hoic_kvparam ALIGN4;
 
 static inline void hoic_set_param(int fd, char* name, char* value)
 {
@@ -312,7 +275,7 @@ typedef struct {
     uint32_t            msgid, msgsize;
     uint32_t            compress, size;
     char                filename[];
-} __attribute__ ((packed)) t_hoic_capture;
+} PACK t_hoic_capture ALIGN4;
 
 
 static inline void hoic_capture(int fd, uint32_t compress, uint32_t size, char* filename)
@@ -332,14 +295,14 @@ typedef struct {
     uint32_t            fpga_date, fpga_svn;
     uint32_t            sysid_date, sysid_id;
     uint32_t            sw_version;
-} __attribute__ ((packed)) t_hoic_version;
+} PACK t_hoic_version ALIGN4;
 
 typedef struct {
     uint32_t            msgid, msgsize;
     uint32_t            fpga_date, fpga_svn;
     uint32_t            sysid_date, sysid_id;
     uint32_t            sw_version;
-} __attribute__ ((packed)) t_hoic_getversion;
+} PACK t_hoic_getversion ALIGN4;
 
 static inline void hoic_getversion(int fd, int fdr, t_hoic_version *ver)
 {
