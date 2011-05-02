11f54627b932cdc501a5332734112e86992b6b24
Thomas Stanger (stth) <stth@zhaw.ch>
2011-05-02 16:17:18 +0200

    hdoip_daemon RSCP: new function to get remote MAC address. ARP ping added.

--- user/hd_over_ip/hdoip_daemon/rscp/rscp_net.c
+++ user/hd_over_ip/hdoip_daemon/rscp/rscp_net.c
@@ -9,8 +9,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
 #include "rscp_net.h"
 #include "rscp_error.h"
+#include "hdoipd.h"
+
+
 
 char *mac_ntoa(unsigned char *ptr)
 {
@@ -57,7 +63,7 @@ int net_get_local_addr(int sock, char* ifn, uint32_t* addr)
     return RSCP_SUCCESS;
 }
 
-int net_get_remote_hwaddr(int sock, char* ifn, uint32_t address, uint8_t* mac)
+int net_read_arp_map(int sock, char* ifn, uint32_t address, uint8_t* mac)
 {
     struct arpreq req;
     struct sockaddr_in *sin;
@@ -84,6 +90,74 @@ int net_get_remote_hwaddr(int sock, char* ifn, uint32_t address, uint8_t* mac)
     return RSCP_SUCCESS;
 }
 
+
+int net_get_remote_hwaddr(int sock, char* ifn, uint32_t address, uint8_t* mac)
+{
+    char *str;
+    char buf[128];
+    struct hostent* gw;
+    struct ifreq ifreq;
+    struct sockaddr_in sin;
+    int ret;
+    uint32_t subnet, dev_ip;
+
+    memset(&ifreq, 0, sizeof(ifreq));
+    strncpy(ifreq.ifr_name, "eth0", sizeof("eth0"));
+
+    /* Read device ip */
+    if((ioctl(sock, SIOCGIFADDR, (void*)&ifreq)) == -1 ) {
+            perror("ioctl(SIOCGIFNETMASK) failed");
+            return RSCP_ERRORNO;
+    }
+
+    memcpy(&sin, &ifreq.ifr_addr, sizeof(struct sockaddr));
+    dev_ip = sin.sin_addr.s_addr;
+
+    /* Read subnet mask */
+    if((ioctl(sock, SIOCGIFNETMASK, (void*)&ifreq)) == -1 ) {
+        perror("ioctl(SIOCGIFNETMASK) failed");
+        return RSCP_ERRORNO;
+    }
+
+    memcpy(&sin, &ifreq.ifr_addr, sizeof(struct sockaddr));
+    subnet = sin.sin_addr.s_addr;
+
+    /* destination is in local network*/
+    if((subnet & address) == (subnet & dev_ip)) {
+        ret = net_read_arp_map(sock, ifn, address, mac);
+        if (ret != RSCP_SUCCESS) {
+            sin.sin_addr.s_addr = address;
+            sprintf(buf, "/bin/busybox arping -c 1 %s", inet_ntoa(sin.sin_addr));
+            system(buf);
+
+            ret = net_read_arp_map(sock, ifn, address, mac);
+        }
+    }
+
+    if(ret == RSCP_SUCCESS) {
+        return RSCP_SUCCESS;
+    }
+
+    /* Switching to gateway */
+    str = reg_get("system-gateway");
+    gw = gethostbyname(str);
+
+    if (!gw) {
+        perror("net_get_remote_hwaddr() gethostbyname failed");
+        return RSCP_ERRORNO;
+    }
+
+    address = *((uint32_t*)gw->h_addr_list[0]);
+    ret = net_read_arp_map(sock, ifn, address, mac);
+
+    if(ret != RSCP_SUCCESS) {
+        sprintf(buf, "/sbin/busybox arping -c 1 %s", str);
+        system(buf);
+        ret =  net_read_arp_map(sock, ifn, address, mac);
+    }
+
+    return ret;
+}
 
 char* str_ntoa(uint32_t address)
 {
