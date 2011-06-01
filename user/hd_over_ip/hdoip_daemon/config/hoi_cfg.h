f0dbe988aea0cde99e6f67ad0710f64f5cb5d25e
Thomas Stanger (stth) <stth@zhaw.ch>
2011-06-01 11:15:38 +0200

    hdoip_daemon config: added function to set/get network parameters on uCLinux.

--- user/hd_over_ip/hdoip_daemon/config/hoi_cfg.h
+++ user/hd_over_ip/hdoip_daemon/config/hoi_cfg.h
@@ -17,10 +17,19 @@
 #include <string.h>
 
 void hoi_cfg_system();
+void hoi_cfg_static_ip();
+void hoi_cfg_dynamic_ip();
+void hoi_cfg_mac_addr(char *ifname, char *mac);
+void hoi_cfg_default_gw(char *gw);
+void hoi_cfg_ip_addr(char *ifname, char *ip, char *netmask);
 
 int hoi_cfg_write(char* filename);
 int hoi_cfg_read(char* filename);
 int hoi_cfg_get_dns_server(char* dns1, char* dns2);
 int hoi_cfg_set_dns_server(char* dns1, char* dns2);
+int hoi_cfg_get_default_gw(char *gw);
+int hoi_cfg_get_ip(char *ip);
+int hoi_cfg_get_subnet(char *subnet);
+
 
 #endif /* HOI_CFG_H_ */
