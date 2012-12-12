31b7df7efc56154facb0c8de516de6cce9d3ee9a
Sascha Montellese <montesas@students.zhaw.ch>
2012-12-12 11:08:57 +0100

    [rtsp] alive_check: get rid of alive-check functionality on the transmitter

--- user/hd_over_ip/hdoip_daemon/alive/alive_check.h
+++ user/hd_over_ip/hdoip_daemon/alive/alive_check.h
@@ -38,18 +38,6 @@ int  alive_check_client_close(t_alive_check *handle);
 int  alive_check_client_handler(t_alive_check *handle, char *hello_msg);
 int  alive_check_client_update(t_alive_check *handle, bool enable, int interval, char *ip, uint16_t port, int broadcast, bool load_new_config);
 
-int  alive_check_server_open(t_alive_check *handle, bool enable, uint16_t port, bool load_new_config);
-int  alive_check_server_close(t_alive_check *handle);
-int  alive_check_server_handler(t_alive_check *handle, char *hello_msg, int msg_length);
-int  alive_check_server_update(t_alive_check *handle, bool enable, uint16_t port, bool load_new_config);
-
-void alive_check_handle_msg_vrb_alive(t_alive_check *handle);
-int  alive_check_test_msg_vrb_alive(char *hello_msg, char *client_ip, uint8_t *edid);
-int  alive_check_response_vrb_alive(char *client_ip);
-int  alive_check_init_msg_vrb_alive();
-void alive_check_start_vrb_alive();
-void alive_check_stop_vrb_alive();
-
 uint8_t return_next_byte(char* s);
 
 #endif /* ALIVE_CHECK_H_ */
