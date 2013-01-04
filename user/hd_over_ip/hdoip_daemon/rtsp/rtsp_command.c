fc3f75337eddbcbdea38ea7253fcaf5b83a5c2ad
Sascha Montellese <montesas@students.zhaw.ch>
2013-01-04 09:14:38 +0100

    [rtsp] add rtsp_request_set_parameter() method

--- user/hd_over_ip/hdoip_daemon/rtsp/rtsp_command.c
+++ user/hd_over_ip/hdoip_daemon/rtsp/rtsp_command.c
@@ -412,6 +412,22 @@ void rtsp_request_get_parameter(t_rtsp_connection* msg, char* uri, char* session
     rtsp_send(msg);
 }
 
+void rtsp_request_set_parameter(t_rtsp_connection* msg, char* uri, char* session, char* media, char* name, char* value)
+{
+    bool has_param = name != NULL && value != NULL && strlen(name) > 0;
+    rtsp_request_line(msg, "SET_PARAMETER", uri, media);
+    if (session != NULL)
+        rtsp_header_session(msg, session);
+    if (has_param)
+        rtsp_header_content_length(msg, strlen(name) + strlen(value) + 1);
+    rtsp_eoh(msg);
+
+    if (has_param)
+        msgprintf(msg, "%s:%s", name, value);
+
+    rtsp_send(msg);
+}
+
 void rtsp_response_play(t_rtsp_connection* msg, char* session, t_rtsp_rtp_format* fmt, t_video_timing* timing)
 {
     rtsp_response_line(msg, RTSP_STATUS_OK, "OK");
