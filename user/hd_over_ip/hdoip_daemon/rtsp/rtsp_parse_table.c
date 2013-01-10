15b2e36e2d08f861db711671b329f4da4b885fb7
Sascha Montellese <montesas@students.zhaw.ch>
2013-01-10 10:12:31 +0100

    [rtsp] cosmetic fixup in rtsp_parse_table.c

--- user/hd_over_ip/hdoip_daemon/rtsp/rtsp_parse_table.c
+++ user/hd_over_ip/hdoip_daemon/rtsp/rtsp_parse_table.c
@@ -38,89 +38,89 @@ const t_map_fnc tab_request_set_parameter[] = {
 
 // SETUP attributes
 const t_map_fnc tab_request_setup[] = {
-	{ "CSeq",	rtsp_parse_ui32,	true,	offsetof(t_rtsp_req_setup, cseq) },
-	{ "Transport",	rtsp_parse_transport,	true,	offsetof(t_rtsp_req_setup, transport) },
-	{ "EDID-Segment",rtsp_parse_edid,	false,	offsetof(t_rtsp_req_setup, edid) },
-    { "HDCP",	rtsp_parse_hdcp,	true,	offsetof(t_rtsp_req_setup, hdcp) },
-	MAP_FNC_NULL
+    { "CSeq",           rtsp_parse_ui32,        true,   offsetof(t_rtsp_req_setup, cseq) },
+    { "Transport",      rtsp_parse_transport,   true,   offsetof(t_rtsp_req_setup, transport) },
+    { "EDID-Segment",   rtsp_parse_edid,        false,  offsetof(t_rtsp_req_setup, edid) },
+    { "HDCP",           rtsp_parse_hdcp,        true,   offsetof(t_rtsp_req_setup, hdcp) },
+    MAP_FNC_NULL
 };
 
 const t_map_fnc tab_response_setup[] = {
-	{ "CSeq",	rtsp_parse_ui32,	true,	offsetof(t_rtsp_rsp_setup, cseq) },
-	{ "Session",	rtsp_parse_str,		true,	offsetof(t_rtsp_rsp_setup, session) },
-	{ "Transport",	rtsp_parse_transport,	true,	offsetof(t_rtsp_rsp_setup, transport) },
-	{ "HDCP",	rtsp_parse_hdcp,	true,	offsetof(t_rtsp_rsp_setup, hdcp) },
-	MAP_FNC_NULL
+    { "CSeq",       rtsp_parse_ui32,        true,    offsetof(t_rtsp_rsp_setup, cseq) },
+    { "Session",    rtsp_parse_str,         true,    offsetof(t_rtsp_rsp_setup, session) },
+    { "Transport",  rtsp_parse_transport,   true,    offsetof(t_rtsp_rsp_setup, transport) },
+    { "HDCP",       rtsp_parse_hdcp,        true,    offsetof(t_rtsp_rsp_setup, hdcp) },
+    MAP_FNC_NULL
 };
 
 // HDCP attributes
 const t_map_fnc tab_request_hdcp[] = {
-	{ "CSeq",	rtsp_parse_ui32,	true,	offsetof(t_rtsp_req_hdcp, cseq) },
-	{ "Session",	rtsp_parse_str,		true,	offsetof(t_rtsp_req_hdcp, session) },
-	{ "ID",		rtsp_parse_str,		true,	offsetof(t_rtsp_req_hdcp, id) },
-	{ "Content",	rtsp_parse_str,		true,	offsetof(t_rtsp_req_hdcp, content) },
-	MAP_FNC_NULL
+    { "CSeq",       rtsp_parse_ui32,        true,    offsetof(t_rtsp_req_hdcp, cseq) },
+    { "Session",    rtsp_parse_str,         true,    offsetof(t_rtsp_req_hdcp, session) },
+    { "ID",         rtsp_parse_str,         true,    offsetof(t_rtsp_req_hdcp, id) },
+    { "Content",    rtsp_parse_str,         true,    offsetof(t_rtsp_req_hdcp, content) },
+    MAP_FNC_NULL
 };
 
 // PLAY attributes
 const t_map_fnc tab_request_play[] = {
-	{ "CSeq",	rtsp_parse_ui32,	true,	offsetof(t_rtsp_req_play, cseq) },
-	{ "Session",	rtsp_parse_str,		true,	offsetof(t_rtsp_req_play, session) },
-	{ "RTP-Format",	rtsp_parse_rtp_format,	true,	offsetof(t_rtsp_req_play, format) },
-	{ "USB",	rtsp_parse_usb,		true,	offsetof(t_rtsp_req_play, usb) },
-	MAP_FNC_NULL
+    { "CSeq",       rtsp_parse_ui32,        true,    offsetof(t_rtsp_req_play, cseq) },
+    { "Session",    rtsp_parse_str,         true,    offsetof(t_rtsp_req_play, session) },
+    { "RTP-Format", rtsp_parse_rtp_format,  true,    offsetof(t_rtsp_req_play, format) },
+    { "USB",        rtsp_parse_usb,         true,    offsetof(t_rtsp_req_play, usb) },
+    MAP_FNC_NULL
 };
 
 const t_map_fnc tab_response_play[] = {
-	{ "CSeq",	rtsp_parse_ui32,	true,	offsetof(t_rtsp_rsp_play, cseq) },
-	{ "Session",	rtsp_parse_str,		true,	offsetof(t_rtsp_rsp_play, session) },
-	{ "Timing",	rtsp_parse_timing,	true,	offsetof(t_rtsp_rsp_play, timing) },
-	{ "RTP-Format", rtsp_parse_rtp_format,	true,	offsetof(t_rtsp_rsp_play, format) },
-	MAP_FNC_NULL
+    { "CSeq",       rtsp_parse_ui32,        true,    offsetof(t_rtsp_rsp_play, cseq) },
+    { "Session",    rtsp_parse_str,         true,    offsetof(t_rtsp_rsp_play, session) },
+    { "Timing",     rtsp_parse_timing,      true,    offsetof(t_rtsp_rsp_play, timing) },
+    { "RTP-Format", rtsp_parse_rtp_format,  true,    offsetof(t_rtsp_rsp_play, format) },
+    MAP_FNC_NULL
 };
 
 // TEARDOWN attributes
 const t_map_fnc tab_request_teardown[] = {
-	{ "CSeq",	rtsp_parse_ui32,	true,	offsetof(t_rtsp_req_teardown, cseq) },
-	{ "Session",	 rtsp_parse_str,	true,	offsetof(t_rtsp_req_teardown, session) },
-        MAP_FNC_NULL
+    { "CSeq",       rtsp_parse_ui32,    true,    offsetof(t_rtsp_req_teardown, cseq) },
+    { "Session",    rtsp_parse_str,     true,    offsetof(t_rtsp_req_teardown, session) },
+    MAP_FNC_NULL
 };
 
 const t_map_fnc tab_response_teardown[] ={
-        { "CSeq",	rtsp_parse_ui32,	true,	offsetof(t_rtsp_rsp_teardown, cseq) },
-        { "Session",	rtsp_parse_str,		true,	offsetof(t_rtsp_rsp_teardown, session) },
-        MAP_FNC_NULL
+    { "CSeq",       rtsp_parse_ui32,    true,    offsetof(t_rtsp_rsp_teardown, cseq) },
+    { "Session",    rtsp_parse_str,     true,    offsetof(t_rtsp_rsp_teardown, session) },
+    MAP_FNC_NULL
 };
 
 // HDCP attributes
 const t_map_fnc tab_response_hdcp[] ={
-        { "CSeq",	rtsp_parse_ui32,	true,	offsetof(t_rtsp_rsp_hdcp, cseq) },
-        { "Session",	rtsp_parse_str, 	true,	offsetof(t_rtsp_rsp_hdcp, session) },
-        { "ID",		rtsp_parse_str, 	true,	offsetof(t_rtsp_rsp_hdcp, id) },
-        { "Content",	rtsp_parse_str,		true,	offsetof(t_rtsp_rsp_hdcp, content) },
-        MAP_FNC_NULL
+    { "CSeq",       rtsp_parse_ui32,    true,    offsetof(t_rtsp_rsp_hdcp, cseq) },
+    { "Session",    rtsp_parse_str,     true,    offsetof(t_rtsp_rsp_hdcp, session) },
+    { "ID",         rtsp_parse_str,     true,    offsetof(t_rtsp_rsp_hdcp, id) },
+    { "Content",    rtsp_parse_str,     true,    offsetof(t_rtsp_rsp_hdcp, content) },
+    MAP_FNC_NULL
 };
 
 // UPDATE attributes
 const t_map_fnc tab_request_update[] ={
-        { "CSeq",	rtsp_parse_ui32,	true,	offsetof(t_rtsp_req_update, cseq) },
-        { "Session",	rtsp_parse_str,		true,	offsetof(t_rtsp_req_update, session) },
-        { "Event",	rtsp_parse_uint32,	true,	offsetof(t_rtsp_req_update, event) },
-        { "RTP-Format",	rtsp_parse_rtp_format,	true,	offsetof(t_rtsp_req_update, format) },
-        MAP_FNC_NULL
+    { "CSeq",       rtsp_parse_ui32,        true,    offsetof(t_rtsp_req_update, cseq) },
+    { "Session",    rtsp_parse_str,         true,    offsetof(t_rtsp_req_update, session) },
+    { "Event",      rtsp_parse_uint32,      true,    offsetof(t_rtsp_req_update, event) },
+    { "RTP-Format", rtsp_parse_rtp_format,  true,    offsetof(t_rtsp_req_update, format) },
+    MAP_FNC_NULL
 };
 
 // PAUSE attributes
 const t_map_fnc tab_request_pause[] ={
-        { "CSeq",	rtsp_parse_ui32,	true,	offsetof(t_rtsp_req_pause, cseq) },
-        { "Session",rtsp_parse_str,		true,	offsetof(t_rtsp_req_pause, session) },
-        MAP_FNC_NULL
+    { "CSeq",       rtsp_parse_ui32,    true,   offsetof(t_rtsp_req_pause, cseq) },
+    { "Session",    rtsp_parse_str,     true,   offsetof(t_rtsp_req_pause, session) },
+    MAP_FNC_NULL
 };
 
 const t_map_fnc tab_response_pause[] ={
-        { "CSeq",	rtsp_parse_ui32,	true,	offsetof(t_rtsp_req_pause, cseq) },
-        { "Session",	rtsp_parse_str,		true,	offsetof(t_rtsp_req_pause, session) },
-        MAP_FNC_NULL
+    { "CSeq",       rtsp_parse_ui32,    true,    offsetof(t_rtsp_req_pause, cseq) },
+    { "Session",    rtsp_parse_str,     true,    offsetof(t_rtsp_req_pause, session) },
+    MAP_FNC_NULL
 };
 
 // the methods
